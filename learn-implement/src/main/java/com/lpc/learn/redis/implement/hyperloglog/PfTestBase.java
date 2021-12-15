c903ca4375e89fd139e443a4117f32e9949f5b1d
lipengcheng20 <lipengcheng3@jd.com>
2021-12-15 10:18:11 +0800

    add: 增加 hyperloglog 的例子

---
+++ learn-implement/src/main/java/com/lpc/learn/redis/implement/hyperloglog/PfTestBase.java
@@ -0,0 +1,70 @@
+package com.lpc.learn.redis.implement.hyperloglog;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Package: com.lpc.learn.redis.implement.hyperloglog
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2021/12/14
+ * Time: 17:39
+ * Description:
+ */
+@Slf4j
+public class PfTestBase {
+    static class BitKeeper {
+        private int maxbits;
+
+        public void random() {
+            long value = ThreadLocalRandom.current().nextLong(2L << 32);
+            int bits = lowZeros(value);
+            if (bits > this.maxbits) {
+                this.maxbits = bits;
+            }
+        }
+
+        private int lowZeros(long value) {
+            int i = 1;
+
+            for (; i < 32; i++) {
+                if (value >> i << i != value) {
+                    break;
+                }
+            }
+            return i - 1;
+        }
+    }
+
+    static class Experiment {
+        private int n;
+        private BitKeeper keeper;
+
+        public Experiment(int n) {
+            this.n = n;
+            this.keeper = new BitKeeper();
+        }
+
+        public void work() {
+            for (int i = 0; i < n; i++) {
+                this.keeper.random();
+            }
+        }
+
+        public void debug() {
+            log.info("实验次数：{} ,log2(n): {} , 低位连续0: {}", this.n,
+                    new BigDecimal(Math.log(this.n) / Math.log(2)).setScale(2, BigDecimal.ROUND_UP).doubleValue(),
+                    this.keeper.maxbits);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1000; i < 100000; i += 100) {
+            Experiment exp = new Experiment(i);
+            exp.work();
+            exp.debug();
+        }
+    }
+}
---
+++ learn-implement/src/main/java/com/lpc/learn/redis/implement/hyperloglog/PfTestOptimization.java
@@ -0,0 +1,84 @@
+package com.lpc.learn.redis.implement.hyperloglog;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Package: com.lpc.learn.redis.implement.hyperloglog
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2021/12/14
+ * Time: 17:54
+ * Description:
+ */
+@Slf4j
+public class PfTestOptimization {
+    static class BitKeeper {
+        private int maxbits;
+
+        public void random(long value) {
+            int bits = lowZeros(value);
+            if (bits > this.maxbits) {
+                this.maxbits = bits;
+            }
+        }
+
+        private int lowZeros(long value) {
+            int i = 1;
+            for (; i < 32; i++) {
+                if (value >> i << i != value) {
+                    break;
+                }
+            }
+            return i - 1;
+        }
+    }
+
+    static class Experiment {
+        private int n;
+        private int k;
+        private BitKeeper[] keepers;
+
+        public Experiment(int n) {
+            this(n, 1024);
+        }
+
+        public Experiment(int n, int k) {
+            this.n = n;
+            this.k = k;
+            this.keepers = new BitKeeper[k];
+            for (int i = 0; i < k; i++) {
+                this.keepers[i] = new BitKeeper();
+            }
+        }
+
+        public void work() {
+            for (int i = 0; i < this.n; i++) {
+                long m = ThreadLocalRandom.current().nextLong(1L << 32);
+                BitKeeper keeper = keepers[(int) (((m & 0xfff0000) >> 16) % keepers.length)];
+                keeper.random(m);
+            }
+        }
+
+        public double estimate() {
+            double sumbitsInverse = 0.0;
+            for (BitKeeper keeper : keepers) {
+                sumbitsInverse += 1.0 / (float) keeper.maxbits;
+            }
+            double avgBits = (float) keepers.length / sumbitsInverse;
+            return Math.pow(2, avgBits) * this.k;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 100000; i < 1000000; i += 100000) {
+            Experiment exp = new Experiment(i);
+            exp.work();
+            double est = new BigDecimal(exp.estimate()).setScale(2,BigDecimal.ROUND_UP).doubleValue();
+            double res = new BigDecimal(Math.abs(est - i) / i).setScale(2,BigDecimal.ROUND_UP).doubleValue();
+            log.info("实验数量:{},估计数量:{},误差:{}", i, est, res);
+        }
+    }
+}
